d8c964cf4dd252666c7bdc3514830ad73d571a51
Mohammad Tat Shahdoost <tatshahdoost@gmail.com>
2023-11-22 16:00:00 +0330

    feat: add not found route and page

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,16 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Routes from "@/routes";
+import Notfound from "@/pages/NotFound";
 import "@/styles/Global.css";
 
 const router = createBrowserRouter([
   {
     path: "/",
     children: Routes,
+  },
+  {
+    path: "*",
+    element: <Notfound />,
   },
 ]);
 
---
+++ src/pages/NotFound/Notfound.tsx
@@ -0,0 +1,9 @@
+const Notfound = () => {
+  return (
+    <div>
+      <h1>404 - Not Found</h1>
+    </div>
+  );
+};
+
+export default Notfound;

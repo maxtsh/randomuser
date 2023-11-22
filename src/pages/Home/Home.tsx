7fe29e390f44e5cfa0669a8978e78a440e7687d6
Mohammad Tat Shahdoost <tatshahdoost@gmail.com>
2023-11-23 00:40:40 +0330

    feat: add skeleton and proper loading status to user card in homepage

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -2,34 +2,44 @@ import { useCustomFetch } from "@/hooks";
 import Button from "@/components/Base/Button";
 import { getUserData } from "./Home.api";
 import styles from "./Home.module.scss";
+import Skeleton from "@/components/Base/Skeleton";
 
 const Home = () => {
-  const { data, error, status } = useCustomFetch(getUserData);
+  const { data, error, isLoading, hasError, refetch } =
+    useCustomFetch(getUserData);
 
   return (
     <div className={styles.container}>
-      {status === "pending" && <p className="loading">Fetching data...</p>}
-      {status === "error" && (
-        <p className="error">{error || "Could not fetch data"}</p>
-      )}
-      {status === "sucess" && (
-        <div className="card">
-          <div className="card__header">
+      {hasError && <p className="error">{error || "Could not fetch data"}</p>}
+      <div className="card">
+        <div className="card__header">
+          {isLoading ? (
+            <Skeleton className="card__header__skeleton" />
+          ) : (
             <img
               className="card__header__avatar"
               src={data?.results?.[0]?.picture?.large}
             />
-          </div>
-          <div className="card__body">
-            <h2>{data?.results?.[0]?.name?.first}</h2>
-            <h3>{data?.results?.[0]?.name?.last}</h3>
-            <h4>{data?.results?.[0]?.location?.country}</h4>
-          </div>
-          <div className="card__footer">
-            <Button>Fetch New User</Button>
-          </div>
+          )}
         </div>
-      )}
+        <div className="card__body">
+          {isLoading && (
+            <>
+              <Skeleton className="card__body__skeleton" />
+              <Skeleton className="card__body__skeleton" />
+              <Skeleton className="card__body__skeleton" />
+            </>
+          )}
+          <h2>{data?.results?.[0]?.name?.first}</h2>
+          <h3>{data?.results?.[0]?.name?.last}</h3>
+          <h4>{data?.results?.[0]?.location?.country}</h4>
+        </div>
+        <div className="card__footer">
+          <Button disabled={isLoading} onClick={refetch}>
+            Fetch New User
+          </Button>
+        </div>
+      </div>
     </div>
   );
 };
